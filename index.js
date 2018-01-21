912033ca3259985c5c16be2d2a5278b2d166756b
MatthewMerrill <MattMerr47@gmail.com>
2018-01-20 18:41:07 -0800

    change deps to bower

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./firebase_key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tutortimetable.firebaseio.com"
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
