f63c476febbf4cf26dcc7dbe66425f0703795303
MatthewMerrill <MattMerr47@gmail.com>
2018-01-20 17:55:17 -0800

    index.html

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
